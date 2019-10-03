b37d5012c2ad275f38620ef865ba4364a3f8b308
추지철 <jc.choo@fitpet.co.kr>
2019-10-03 16:36:05 +0900

    Update WebviewActivity

--- app/src/main/java/kr/gdg/sooksook/util/extensions/ActivityExt.kt
+++ app/src/main/java/kr/gdg/sooksook/util/extensions/ActivityExt.kt
@@ -3,10 +3,30 @@ package kr.gdg.sooksook.util.extensions
 import android.app.Activity
 import androidx.annotation.ColorRes
 import android.view.WindowManager
+import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 fun Activity.changeStatusBar(@ColorRes color: Int) {
     val window = window
     window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
     window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
     window.statusBarColor = this.getColorById(color)
+}
+
+fun AppCompatActivity.actionBarHide() {
+    supportActionBar?.hide()
+}
+
+fun AppCompatActivity.setActionBarHome(toolbar: Toolbar, @DrawableRes res: Int? = null) {
+    setSupportActionBar(toolbar)
+    supportActionBar?.apply {
+        setDisplayHomeAsUpEnabled(true)
+        setHomeButtonEnabled(true)
+        res?.let {
+            setHomeAsUpIndicator(it)
+        } ?: run {
+            setHomeAsUpIndicator(null)
+        }
+    }
 }
--- app/src/main/java/kr/gdg/sooksook/view/activity/WebViewActivity.kt
+++ app/src/main/java/kr/gdg/sooksook/view/activity/WebViewActivity.kt
@@ -6,37 +6,50 @@ import kr.gdg.sooksook.base.BaseActivity
 import kr.gdg.sooksook.databinding.ActivityWebviewBinding
 import kr.gdg.sooksook.util.extensions.changeStatusBar
 import android.content.Intent
+import android.view.MenuItem
+import kr.gdg.sooksook.R
+import kr.gdg.sooksook.common.Dlog
+import kr.gdg.sooksook.util.extensions.setActionBarHome
 
 class WebViewActivity : BaseActivity<ActivityWebviewBinding>() {
     override val getContentView: Int
-        get() = kr.gdg.sooksook.R.layout.activity_webview
+        get() = R.layout.activity_webview
 
     private lateinit var url: String
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun initView() {
-        title = ""
-        changeStatusBar(android.R.color.black)
+        Dlog.i("initView")
+
+        setActionBarHome(getBinding().toolbar, R.drawable.bar_grey)
 
         url = intent.getStringExtra("url")
 
-        getBinding().webView.apply {
+        getBinding().webView.run {
             webViewClient = WebViewClient()
             clearCache(true)
             clearHistory()
             settings.javaScriptEnabled = true
             settings.javaScriptCanOpenWindowsAutomatically = true
             settings.domStorageEnabled = true
+            loadUrl(url)
         }
 
-        getBinding().webView.loadUrl(url)
-        getBinding().webViewIvSend.setOnClickListener { onClickActionSend() }
+//        getBinding().webView.loadUrl(url)
+        getBinding().webViewIvSend.setOnClickListener {
+            startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, url)
+            }, "친구에게 공유하기"))
+        }
     }
 
-    private fun onClickActionSend() {
-        startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND).apply {
-            type = "text/plain"
-            putExtra(Intent.EXTRA_TEXT, url)
-        }, "친구에게 공유하기"))
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when(item?.itemId) {
+        android.R.id.home -> {
+            finish()
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
     }
 }
